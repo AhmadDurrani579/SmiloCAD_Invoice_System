25a4c6e13925ae3b47c01f1b00eda5d1dd574dab
AhmadDurrani579 <ahmaddurranitrg@gmail.com>
2026-04-08 17:39:51 +0100

    feat: add invoice creation logic with SQLAlchemy

--- backend/README.md
+++ backend/README.md
@@ -6,4 +6,5 @@ colorTo: indigo
 sdk: docker
 pinned: false
 app_port: 7860
+docker_path: backend/Dockerfile
 ---
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
+import os
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from core.database import engine, Base
 from api.invoices import router as invoice_router
 
@@ -8,12 +10,24 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SmiloCAD Invoice API")
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# 1. Route to serve the index.html
+@app.get("/")
+async def read_index():
+    # Looks for frontend/index.html
+    return FileResponse("../frontend/index.html")
+
+# 2. Mount the CSS and JS folders so the HTML can find them
+# This matches your folder names: frontend/css and frontend/js
+app.mount("/css", StaticFiles(directory="../frontend/css"), name="css")
+app.mount("/js", StaticFiles(directory="../frontend/js"), name="js")
 
 # Include the routes from the api folder
 app.include_router(invoice_router)
