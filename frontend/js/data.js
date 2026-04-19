6b4a2d008c7fe69d0cba948cff34ccf69d5e4251
AhmadDurrani579 <ahmaddurranitrg@gmail.com>
2026-04-19 16:10:35 +0100

    Change the spelling mistake

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -2,7 +2,7 @@ const LAB = {
   name: "SmiloCAD Dental Laboratory",
   phone: "0328-9577771",
   address: "Al Anayat Plaza, G11 Markaz Islamabad",
-  tech: "Dr. Sajid",
+  tech: "Dt. Sajid",
 };
 
 const SERVICES = [
